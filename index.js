b6632b6acead29016b592b9a112ff84f577b6728
ngigi11287 <ngigi.11287@gmail.com>
2024-11-29 09:49:18 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let responce;
+  if (feet <= 400){
+    return responce = 'This one is on me!';
+  }else if(feet > 400 && feet < 2000){
+    return responce = 'That will be twenty bucks.';
+  }else if (feet > 2000 && feet <= 2500){
+    return responce = 'I will gladly take your thirty bucks.';
+  }else{
+    return responce = 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+scuberGreetingForFeet(30);
+console.log(responce)
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+   let  message = city === "NYC" ? "Ok, sounds good." : "No go.";
+   return message;
 }
+console.log(ternaryCheckCity(""));
+
+
+
+
+function switchOnCharmFromTip(a){
+  // Write your code here!
+  
+  let reply;
+switch(true){
+  case a === "generous" :
+    reply ='Thank you so much.';
+    break;
+
+    case a === "not as generous" :
+      reply = "Thank you.";
+    break;
+
+    default :
+          reply = "Bye. ";
+          break;
+}
+  return reply;
+}
+console.log(switchOnCharmFromTip())
